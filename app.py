647785a35358b75a4b67a607bc1f40c0e21a3b90
Fabio <email@fabioac.com>
2023-04-01 21:08:34 -0400

    Latest changes

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import openai
 import random
+import os
+import json
 
 # Initialize OpenAI API key
-openai.api_key = "YOUR_API_KEY"
+if "openai.api_key" not in globals():
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+    print("OpenAI API key initialized from environment variable.")
+else:
+    openai.api_key = "YOUR_API_KEY"
+    print("OpenAI API key initialized from script.")
 
 # Function to generate a prompt based on a user-defined topic
 def generate_prompt(topic):
@@ -34,33 +41,51 @@ def generate_prompt(topic):
 
 # Function to generate a ChatGPT response
 def generate_response(prompt):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            engine="gpt-3.5-turbo",
+            prompt=prompt,
+            n=1,
+            stop=None,
+            max_tokens=1024,
+            temperature=0.7,
+        )
+    except openai.error.InvalidRequestError as e:
+        print("Error:", e)
+        return "Error: " + str(e)
 
-    return response.choices[0].text.strip()
+    # Extract the chatbot's response from the API response
+    chat_response = response.choices[0].text.strip()
+
+    return chat_response
+
 
 # Main function to generate prompts and responses
 def main():
-    # Get user input for topic, default to empty string if no topic provided
-    topic = input("Enter a topic for the prompt (press Enter for a random prompt): ").strip()
+    try:
+        
+        # Get user input for topic, default to empty string if no topic provided
+        topic = input("Enter a topic for the prompt (press Enter for a random prompt): ").strip()
 
-    if topic:
-        prompt = generate_prompt(topic)
-    else:
-        prompt = generate_prompt("a random topic")
+        if topic:
+            prompt = generate_prompt(topic)
+        else:
+            prompt = generate_prompt("a random topic")
 
-    # Generate ChatGPT response based on prompt
-    response = generate_response(prompt)
+        # Generate ChatGPT response based on prompt
+        response = generate_response(prompt)
 
-    # Print prompt and response
-    print(f"Prompt: {prompt}")
-    print(f"Response: {response}")
+        # Print prompt and response
+        print(f"Prompt: {prompt}")
+        print(f"Response: {response}")
+        
+        # Save prompt and response to a json file
+        with open("prompt_response.json", "w") as f:
+            f.write(f'{{"prompt": "{prompt}", "response": "{response}"}}')
+        
+    except KeyboardInterrupt:
+        print("Exiting...")
+        exit()
 
 if __name__ == "__main__":
     main()
